ba061561be40c302cf1b149c974f665da43e2b3d
Igopk <101907629+Igopk@users.noreply.github.com>
2022-03-25 20:31:22 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+BOT_TOKEN = '5232504383:AAEc2LfEJHfvLol5QFhSsubXy7YbCmBv7LE' #token de tu bot
+API_ID = 14681595 # lo creas en https://my.telegram.org/
+API_HASH = 'a86730aab5c59953c424abb4396d32d5'  # lo creas en https://my.telegram.org/
+OWNER = [1805924744]
+CHAT_ID = '-1001666279733'
+
+# DATOS DEL MOODLE
+USUARIO = 'jsataury '
+MOODLE_PASSWORD = 'chichi123-'
+USUARIO_ID = 3588 #Tu id
+ZIP_MB = 399
+MOODLE_URL = 'https://moodle.uclv.edu.cu'
+
+# CUENTA DE MEGA - este bot sirve par mega tambien
+GMAIL_MEGA = 'AQUI TU GMAIL' 
+PASS_MEGA = 'AQUI TU PASS'
